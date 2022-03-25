f8531937911f5810ec0618e2c1c7670e4fa2d18d
chul0721 <chul070721@naver.com>
2022-03-25 22:36:23 +0900

    fix: fix alignment to responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,39 +10,53 @@ const Home: NextPage = () => {
     <>
       <Header />
       <section>
-        <div className="flex flex-row h-screen">
-          <div className="flex flex-col justify-center space-y-1 absolute h-4/5 pl-8 sm:pl-10 md:pl-24">
-            <motion.div
-              className="flex flex-row pl-1 font-inter_thin"
-              initial={{ y: -50, opacity: 0 }}
-              whileInView={{ y: 0, opacity: 1 }}
-              viewport={{ once: true }}
-              transition={{
-                type: 'spring',
-                delay: 0.2,
-                stiffness: 250,
-                damping: 100,
-                mass: 1.8
-              }}
-            >
-              <p className="text-2xl sm:text-4xl">Hi, I&apos;m</p>
-            </motion.div>
-            <motion.div
-              initial={{ x: -40, opacity: 0 }}
-              whileInView={{ x: 0, opacity: 1 }}
-              viewport={{ once: true }}
-              transition={{
-                type: 'spring',
-                delay: 0.2,
-                stiffness: 250,
-                damping: 100,
-                mass: 1.8
-              }}
-              className="flex flex-row flex-wrap items-center font-inter_bold text-5xl sm:text-6xl sm:space-x-4"
-            >
-              <p className="w-full sm:w-auto">Byungchul</p>
-              <p className="bg-black my-2 text-white">Kim.</p>
-            </motion.div>
+        <div className="grid content-center h-screen">
+          <div className="flex flex-col justify-center space-y-1 absolute h-4/5 w-full">
+            <div className="flex flex-col self-center w-11/12 md:w-5/6 lg:w-4/5 2xl:w-3/5">
+              <motion.div
+                className="flex flex-row pl-1 font-inter_thin"
+                initial={{ y: -50, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{
+                  type: 'spring',
+                  delay: 0.4,
+                  stiffness: 250,
+                  damping: 100,
+                  mass: 1.8
+                }}
+              >
+                <p className="text-2xl sm:text-4xl">Hi, I&apos;m</p>
+              </motion.div>
+              <motion.div
+                initial={{ x: -40, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{
+                  type: 'spring',
+                  delay: 0.2,
+                  stiffness: 250,
+                  damping: 100,
+                  mass: 1.8
+                }}
+                className="flex flex-row flex-wrap items-center font-inter_bold text-5xl sm:text-6xl sm:space-x-4"
+              >
+                <p className="w-full sm:w-auto">Byungchul</p>
+                <p className="bg-black my-2 text-white">Kim.</p>
+              </motion.div>
+              <motion.div
+                initial={{ x: -40, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{
+                  type: 'spring',
+                  delay: 0.5,
+                  stiffness: 250,
+                  damping: 100,
+                  mass: 1.8
+                }}
+                className="pl-1 text-lg"
+              >
+                프로그래머를 꿈꾸는 평범한 학생 개발자입니다. 👋
+              </motion.div>
+            </div>
           </div>
         </div>
       </section>
@@ -51,7 +65,6 @@ const Home: NextPage = () => {
           <motion.div
             initial="hidden"
             whileInView="visible"
-            viewport={{ once: true }}
             transition={{
               delay: 0.2,
               duration: 1
@@ -60,9 +73,23 @@ const Home: NextPage = () => {
               visible: { opacity: 1, x: 0 },
               hidden: { opacity: 0, x: -60 }
             }}
-            className="pl-8 sm:pl-10 md:pl-20 font-inter_bold text-4xl"
+            className="pl-8 sm:pl-10 md:pl-20 2xl:pl-40 font-inter_bold text-4xl"
           >
             Stack
+          </motion.div>
+          <motion.div
+            initial={{ x: -40, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{
+              type: 'spring',
+              delay: 0.5,
+              stiffness: 250,
+              damping: 100,
+              mass: 1.8
+            }}
+            className="pl-8 sm:pl-10 md:pl-20 2xl:pl-40 py-1 pr-8"
+          >
+            제가 사용하는 기술들이에요. 마우스를 위에 올리면 상세 정보를 확인하실 수 있어요.
           </motion.div>
           <div className="flex flex-row flex-grow flex-wrap justify-center pt-4">
             {Object.entries(skills).map(([_, skill]) => (
@@ -74,11 +101,10 @@ const Home: NextPage = () => {
         </div>
       </section>
       <section>
-        <div className="grid content-center space-y-24 h-screen">
+        <div className="grid content-center h-screen">
           <motion.div
             initial="hidden"
             whileInView="visible"
-            viewport={{ once: true }}
             transition={{
               delay: 0.2,
               duration: 1
@@ -87,11 +113,25 @@ const Home: NextPage = () => {
               visible: { opacity: 1, x: 0 },
               hidden: { opacity: 0, x: -60 }
             }}
-            className="pl-8 sm:pl-10 md:pl-20 font-inter_bold text-4xl"
+            className="pl-8 sm:pl-10 md:pl-20 2xl:pl-40 font-inter_bold text-4xl"
           >
             Projects
           </motion.div>
-          <div className="flex flex-row flex-grow flex-wrap justify-center md:space-x-8">
+          <motion.div
+            initial={{ x: -40, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{
+              type: 'spring',
+              delay: 0.5,
+              stiffness: 250,
+              damping: 100,
+              mass: 1.8
+            }}
+            className="pl-8 sm:pl-10 md:pl-20 2xl:pl-40 py-1 pr-8"
+          >
+            제 토이 프로젝트들이에요. 아이템을 클릭하면 외부 사이트로 이동해요.
+          </motion.div>
+          <div className="flex flex-row flex-grow flex-wrap justify-center md:space-x-8 pt-24">
             {Object.entries(projects).map(([_, project]) => (
               <span key={project.name}>
                 <ProjectCard
