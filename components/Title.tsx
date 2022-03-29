e377cfed6c766b5d0ae8c37d7e66eae659e8cb20
chul0721 <chul070721@naver.com>
2022-03-29 16:43:52 +0900

    feat: give space to components

--- components/Title.tsx
+++ components/Title.tsx
@@ -15,7 +15,7 @@ const Title: FC<titleProps> = (props) => {
         visible: { opacity: 1, x: 0 },
         hidden: { opacity: 0, x: -60 }
       }}
-      className="pl-8 sm:pl-10 md:pl-20 2xl:pl-40 font-inter_bold text-4xl"
+      className="pl-4 sm:pl-10 md:pl-20 2xl:pl-40 font-inter_bold text-4xl"
     >
       {props.name}
     </motion.div>
